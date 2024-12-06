e792fe275c3cd0dc0aa1d492ff7462fb6903bc40
DMprog <crazyprogdm@mail.ru>
2024-12-06 15:33:54 +0300

    Xd

---
+++ pkg/handler/Item.go
@@ -0,0 +1,23 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+func (h *Handler) CreateItem(c *gin.Context) {
+
+}
+
+func (h *Handler) GetAllItems(c *gin.Context) {
+
+}
+
+func (h *Handler) GetItemById(c *gin.Context) {
+
+}
+
+func (h *Handler) updateItem(c *gin.Context) {
+
+}
+
+func (h *Handler) deleteItem(c *gin.Context) {
+
+}
---
+++ pkg/handler/handler.go
@@ -0,0 +1,46 @@
+package handler
+
+import (
+	"ToDORestApi/pkg/service"
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	services *service.Service
+}
+
+func NewHandler(services *service.Service) *Handler {
+	return &Handler{services: services}
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+
+	auth := router.Group("/auth")
+	{
+		auth.POST("/sign-up", h.signUp)
+		auth.POST("/sign-in", h.signIn)
+	}
+
+	api := router.Group("/api")
+	{
+		lists := api.Group("/lists")
+		{
+			lists.POST("/", h.CreateList)
+			lists.GET("/", h.GetAllLists)
+			lists.GET("/:id", h.GetListById)
+			lists.PUT("/:id", h.updateList)
+			lists.DELETE("/:id", h.deleteList)
+
+			items := lists.Group(":id/items")
+			{
+				items.POST("/", h.CreateItem)
+				items.GET("/", h.GetAllItems)
+				items.GET("/:item_id", h.GetItemById)
+				items.PUT("/:item_id", h.updateItem)
+				items.DELETE("/:item_id", h.deleteItem)
+			}
+		}
+	}
+	return router
+}
